a5f7af85c0bfde992c4fbe1aa240eee768546d6b
TheFBOTI <martyn.b.se@gmail.com>
2026-03-11 10:51:08 +0000

    feat(take-home): updated logic to include interest calculations mentioned in readme, refactored whilst there

--- java/src/main/java/org/ikigaidigital/timedeposit/TimeDepositCalculator.java
+++ java/src/main/java/org/ikigaidigital/timedeposit/TimeDepositCalculator.java
@@ -5,26 +5,39 @@ import java.math.RoundingMode;
 import java.util.List;
 
 public class TimeDepositCalculator {
+//Not  allowed to update method signature - but for readability I would have updated the xs to a different name something generic like
+    // ListOfTimeDeposits
     public void updateBalance(List<TimeDeposit> xs) {
-        for (int i = 0; i < xs.size(); i++) {
-            double interest = 0;
 
-            if (xs.get(i).getDays() > 30) {
-                if (xs.get(i).getPlanType().equalsIgnoreCase("student")) {
-                    if (xs.get(i).getDays() < 366) {
-                        interest += xs.get(i).getBalance() * 0.03 / 12;
-                    }
-                } else if (xs.get(i).getPlanType().equalsIgnoreCase("premium")) {
-                    if (xs.get(i).getDays() > 45) {
-                        interest += xs.get(i).getBalance() * 0.05 / 12;
-                    }
-                } else if (xs.get(i).getPlanType().equalsIgnoreCase("basic")) {
-                    interest += xs.get(i).getBalance() * 0.01 / 12;
+        for (TimeDeposit timedeposit : xs) {
+            double interest = 0;
+            int days = timedeposit.getDays();
+            double balance = timedeposit.getBalance();
+
+            if (days > 30) {
+                switch (timedeposit.getPlanType().toLowerCase()) {
+                    case "basic":
+                        interest = balance * 0.01 / 12;
+                        break;
+
+                    case "student":
+                        // Probably should confirm in reality - is a year 365, or does it change on a leap year.
+                        // as an upgrade we could add ActiveDate on the TimeDeposit object which would allow us to use LocalDate time to add a year (which accounts for the leap year)
+                        if (days <= 365) {
+                            interest = balance * 0.03 / 12;
+                        }
+                        break;
+
+                    case "premium":
+                        if (days > 45) {
+                            interest = balance * 0.05 / 12;
+                        }
+                        break;
                 }
             }
-
-            double a2d = xs.get(i).getBalance() + (new BigDecimal(interest).setScale(2, RoundingMode.HALF_UP)).doubleValue();
-            xs.get(i).setBalance(a2d);
+            double a2d = timedeposit.getBalance() + (new BigDecimal(interest).setScale(2, RoundingMode.HALF_UP)).doubleValue();
+            timedeposit.setBalance(a2d);
         }
     }
+
 }
